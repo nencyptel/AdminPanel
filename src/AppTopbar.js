762dd8ab408f84f213764d25c00ce67c01e12cef
nencyptel <nency.patel@infilon.com>
2022-09-19 17:34:15 +0530

    final

--- src/AppTopbar.js
+++ src/AppTopbar.js
@@ -1,24 +1,6 @@
 import React ,{useRef} from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
-<<<<<<< HEAD
-import { Dropdown } from 'primereact/dropdown';
-import { Menu } from 'primereact/menu';
-import { Button } from 'primereact/button';
-import { Toast } from 'primereact/toast';
-const { NavDropdown , Nav} = require('react-bootstrap');
-
-
-export const AppTopbar = (props) => {
-
-   
-let items = [
-    {label: 'New', icon: 'pi pi-fw pi-plus'},
-    {label: 'Delete', icon: 'pi pi-fw pi-trash'}
-];
-const menu = useRef(null);
-
-=======
 // import 'react-dropdown/style.css';
 import { Menu } from 'primereact/menu';
 import { Button } from 'primereact/button';
@@ -31,7 +13,6 @@ export const AppTopbar = (props) => {
 
     const menu = useRef(null);
     
->>>>>>> 4b197349c26642a1673a365bfcbefab0da6089c9
     return (
         <div className="layout-topbar">
             <Link to="/" className="layout-topbar-logo">
@@ -61,22 +42,11 @@ export const AppTopbar = (props) => {
                         </button>
                     </li>
                     <li>
-<<<<<<< HEAD
-
-                    {/* <Dropdown className="pi pi-fw pi-user" options={citySelectItems}></Dropdown> */}
-        
-                        <button className="p-link layout-topbar-button" onClick={props.onMobileSubTopbarMenuClick}>
-                        <Menu model={items} popup ref={menu} />
-                        <Button label="" icon="pi pi-user" onClick={(event) => menu.current.toggle(event)}/>
-                        <span>Profile</span>                                           
-                        </button>
-=======
                     <button className="p-link topbar-button " style={{display:"flex",marginLeft:"6px"}}  onClick={props.onMobileSubTopbarMenuClick}>
                         <Menu model={items} popup ref={menu} />
                         <Button label="" icon="pi pi-user" style={{color:"grey",border:"none",background:"transparent",display:"flex"}} onClick={(event) => menu.current.toggle(event)}/>   
                         <span className="span1">Profile</span>                                                                      
                     </button>   
->>>>>>> 4b197349c26642a1673a365bfcbefab0da6089c9
                     </li>
                 </ul>
         </div>
